9da6cbe19600d0eb398561f799d2ff28dbd9a0c7
Konstantin Lazarev <le6oww5k1@gmail.com>
2017-04-29 22:01:28 +0500

    Draw board and snake in it

--- src/cljs/snake_game/core.cljs
+++ src/cljs/snake_game/core.cljs
@@ -1,11 +1,30 @@
 (ns snake-game.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as r]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defn cell [id color]
+  ^{:key id} [:div.board-cell {:style {:backgroundColor color}}])
 
-(defn greeting []
-  [:h1 (:text @app-state)])
+(defonce snake-state (r/atom {:direction "right"
+                              :body-coordinates #{[0 3] [1 3] [2 3] [2 4]}}))
 
-(reagent/render [greeting] (js/document.getElementById "app"))
+(defn snake-coordinates []
+  (@snake-state :body-coordinates))
+
+(defn snake-occupied? [x y]
+  ((snake-coordinates) [x y]))
+
+(defn board-component [size]
+  [:div.board {:class (str "board-" size)}
+   (for [y (range size)
+         x (range size)
+         :let [id (str x y)
+               snake-cell? (snake-occupied? x y)
+               cell-color (if snake-cell? "black" "white")]]
+     [cell id cell-color])])
+
+(defn board-20-component []
+  (board-component 20))
+
+(r/render [board-20-component] (js/document.getElementById "app"))
